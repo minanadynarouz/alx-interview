fc2a10680811580e270157f7ab77be531f747b9b
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-10 00:58:44 +0300

    Moduling the code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,7 +11,6 @@ def canUnlockAll(boxes):
 
     n = len(boxes)
     opened = [False] * n
-    opened[0] = True
 
     for box in boxes:
         for key in box:
@@ -22,3 +21,9 @@ def canUnlockAll(boxes):
         return True
     else:
         return False
+
+
+boxes = [[7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3], [
+    7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7], [4, 2, 9, 6, 6, 5, 5], ]
+
+print(canUnlockAll(boxes))
