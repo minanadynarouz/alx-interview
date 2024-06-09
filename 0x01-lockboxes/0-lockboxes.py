679699c6fe8156c4adaabe49f535ba8c29586f45
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-10 01:01:00 +0300

    Moduling the code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -22,9 +22,3 @@ def canUnlockAll(boxes):
                 stack.append(key)
 
     return all(opened)
-
-
-boxes = [[7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3], [
-    7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7], [4, 2, 9, 6, 6, 5, 5], ]
-
-print(canUnlockAll(boxes))
