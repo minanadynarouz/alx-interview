ec4cbb5e6aac4cd02bf9b13774c2e1792cfb54b8
Mina Nady Narouz <131834381+minanadynarouz@users.noreply.github.com>
2024-06-10 00:54:11 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,18 @@
+#!/usr/bin/python3
+
 def canUnlockAll(boxes):
+    """Determines if all the boxes can be unlocked"""
+
     n = len(boxes)
     opened = [False] * n
     opened[0] = True
 
     for box in boxes:
         for key in box:
-            if opened[key]:
+            if key < n:
                 opened[key] = True
 
     if all(opened):
         return True
-    return False
-
-
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+    else:
+        return False
