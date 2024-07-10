9002e2199d583105b6c4a7c5e805751ae25cba97
minanadynarouz <mina.n.narouz@gmail.com>
2024-07-09 22:06:45 -0700

    Task 0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const urlPeople = data.characters;
+    for (let i = 0; i < urlPeople.length; i++) {
+      request(urlPeople[i], (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const data = JSON.parse(body);
+          console.log(data.name);
+        }
+      });
+    }
+  }
+});
