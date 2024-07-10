908e8f4737a8a96111aa8d27580c16bd13e5be12
minanadynarouz <mina.n.narouz@gmail.com>
2024-07-09 22:26:51 -0700

    Task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -10,15 +10,20 @@ request(url, (error, response, body) => {
   } else {
     const data = JSON.parse(body);
     const urlPeople = data.characters;
-    for (let i = 0; i < urlPeople.length; i++) {
-      request(urlPeople[i], (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const data = JSON.parse(body);
-          console.log(data.name);
-        }
-      });
-    }
+    const peopleName = urlPeople.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promError, _, promBody) => {
+          if (promError) {
+            reject(promError);
+          } else {
+            resolve(JSON.parse(promBody).name);
+          }
+        })
+      })
+    );
+    Promise.all(peopleName)
+      .then(names => console.log(names.join('\n')))
+      .catch(Err => console.log(Err));
   }
 });
+
