85bf62c1c1dd68ed11737288cc97e5d20543d0d6
minanadynarouz <mina.n.narouz@gmail.com>
2024-12-03 05:52:14 +0200

    Amending function

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,23 +4,30 @@
 
 def island_perimeter(grid):
     """Return the perimiter of an island.
+
     The grid represents water by 0 and land by 1.
+
     Args:
         grid (list): A list of list of integers representing an island.
+
     Returns:
         The perimeter of the island defined in grid.
     """
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:  # If it's land
+                # Check all four directions
+                if i == 0 or grid[i - 1][j] == 0:  # Up
+                    perimeter += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:  # Down
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:  # Left
+                    perimeter += 1
+                if j == cols - 1 or grid[i][j + 1] == 0:  # Right
+                    perimeter += 1
+
+    return perimeter
